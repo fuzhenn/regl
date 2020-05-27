a24a4089a51b58022e361b3af43d09c41fe1dff0
fuzhenn <fuzhen@maptalks.org>
2020-05-27 18:48:32 +0800

    add dist and add version script in package.json

--- dist/regl.js
+++ dist/regl.js
@@ -1151,6 +1151,157 @@ var pool = createPool()
 // zero pool for initial zero data
 pool.zero = createPool()
 
+var GL_DEPTH_COMPONENT = 0x1902
+var GL_DEPTH_STENCIL = 0x84F9
+var HALF_FLOAT_OES = 0x8D61
+
+var gl2Extensions = {
+  'WEBGL_depth_texture': {
+    'UNSIGNED_INT_24_8_WEBGL': 0x84FA
+  },
+  'OES_element_index_uint': {},
+  'OES_texture_float': {},
+  'OES_texture_float_linear': {},
+  'OES_texture_half_float': {
+    'HALF_FLOAT_OES': 0x8D61
+  },
+  'OES_texture_half_float_linear': {},
+  'EXT_color_buffer_float': {},
+  'OES_standard_derivatives': {},
+  'EXT_frag_depth': {},
+  'EXT_blend_minmax': {
+    'MIN_EXT': 0x8007,
+    'MAX_EXT': 0x8008
+  },
+  'EXT_shader_texture_lod': {}
+}
+
+var gl2 = {
+  gl2: function (gl, extensions) {
+    gl[this.versionProperty] = 2
+    for (var p in gl2Extensions) {
+      extensions[p.toLowerCase()] = gl2Extensions[p]
+    }
+
+    // to support float and half-float textures
+    gl.getExtension('EXT_color_buffer_float')
+
+    // mocks of draw buffers's functions
+    extensions['webgl_draw_buffers'] = {
+      drawBuffersWEBGL: function () {
+        return gl.drawBuffers.apply(gl, arguments)
+      },
+      
+      // 'COLOR_ATTACHMENT0_WEBGL': 0x8CE0,
+      // 'COLOR_ATTACHMENT1_WEBGL': 0x8CE1,
+      // 'COLOR_ATTACHMENT2_WEBGL': 0x8CE2,
+      // 'COLOR_ATTACHMENT3_WEBGL': 0x8CE3,
+      // 'COLOR_ATTACHMENT4_WEBGL': 0x8CE4,
+      // 'COLOR_ATTACHMENT5_WEBGL': 0x8CE5,
+      // 'COLOR_ATTACHMENT6_WEBGL': 0x8CE6,
+      // 'COLOR_ATTACHMENT7_WEBGL': 0x8CE7,
+      // 'COLOR_ATTACHMENT8_WEBGL': 0x8CE8,
+      // 'COLOR_ATTACHMENT9_WEBGL': 0x8CE9,
+      // 'COLOR_ATTACHMENT10_WEBGL': 0x8CE10,
+      // 'COLOR_ATTACHMENT11_WEBGL': 0x8CE11,
+      // 'COLOR_ATTACHMENT12_WEBGL': 0x8CE12,
+      // 'COLOR_ATTACHMENT13_WEBGL': 0x8CE13,
+      // 'COLOR_ATTACHMENT14_WEBGL': 0x8CE14,
+      // 'COLOR_ATTACHMENT15_WEBGL': 0x8CE15,
+
+      // 'DRAW_BUFFER0_WEBGL': 0x8825,
+      // 'DRAW_BUFFER1_WEBGL': 0x8826,
+      // 'DRAW_BUFFER2_WEBGL': 0x8827,
+      // 'DRAW_BUFFER3_WEBGL': 0x8828,
+      // 'DRAW_BUFFER4_WEBGL': 0x8829,
+      // 'DRAW_BUFFER5_WEBGL': 0x882A,
+      // 'DRAW_BUFFER6_WEBGL': 0x882B,
+      // 'DRAW_BUFFER7_WEBGL': 0x882C,
+      // 'DRAW_BUFFER8_WEBGL': 0x882D,
+      // 'DRAW_BUFFER9_WEBGL': 0x882E,
+      // 'DRAW_BUFFER10_WEBGL': 0x882F,
+      // 'DRAW_BUFFER11_WEBGL': 0x8830,
+      // 'DRAW_BUFFER12_WEBGL': 0x8831,
+      // 'DRAW_BUFFER13_WEBGL': 0x8832,
+      // 'DRAW_BUFFER14_WEBGL': 0x8833,
+      // 'DRAW_BUFFER15_WEBGL': 0x8834,
+      // 'MAX_COLOR_ATTACHMENTS_WEBGL': 0x8CDF,
+      // 'MAX_DRAW_BUFFERS_WEBGL': 0x8824
+    }
+
+    // mocks of vao functions
+    extensions['oes_vertex_array_object'] = {
+      'VERTEX_ARRAY_BINDING_OES':   0x85B5,
+      'createVertexArrayOES': function () {
+        return gl.createVertexArray()
+      },
+      'deleteVertexArrayOES': function () {
+        return gl.deleteVertexArray.apply(gl, arguments)
+      },
+      'isVertexArrayOES': function () {
+        return gl.isVertexArray.apply(gl, arguments)
+      },
+      'bindVertexArrayOES': function () {
+        return gl.bindVertexArray.apply(gl, arguments)
+      }
+    }
+
+    extensions['angle_instanced_arrays'] = {
+      'VERTEX_ATTRIB_ARRAY_DIVISOR_ANGLE': 0x88FE,
+      'drawArraysInstancedANGLE': function () {
+        return gl.drawArraysInstanced.apply(gl, arguments)
+      },
+      'drawElementsInstancedANGLE': function () {
+        return gl.drawElementsInstanced.apply(gl, arguments)
+      },
+      'vertexAttribDivisorANGLE': function () {
+        return gl.vertexAttribDivisor.apply(gl, arguments)
+      }
+    }
+  },
+
+  versionProperty: '___regl_gl_version___',
+
+  getInternalFormat: function (gl, format, type) {
+    if (gl[this.versionProperty] !== 2) {
+      return format
+    }
+    // webgl2
+    // reference:
+    // https://webgl2fundamentals.org/webgl/lessons/webgl-data-textures.html
+    if (format === GL_DEPTH_COMPONENT) {
+      // gl.DEPTH_COMPONENT24
+      return 0x81A6
+    } else if (format === GL_DEPTH_STENCIL) {
+      // gl.DEPTH24_STENCIL8
+      return 0x88F0
+    } else if (type === HALF_FLOAT_OES && format === gl.RGBA) {
+      // gl.RGBA16F
+      return 0x881A
+    } else if (type === HALF_FLOAT_OES && format === gl.RGB) {
+      // gl.RGB16F
+      return 0x881B
+    } else if (type === gl.FLOAT && format === gl.RGBA) {
+      // gl.RGBA32F
+      return 0x8814
+    } else if (type === gl.FLOAT && format === gl.RGB) {
+      // gl.RGB32F
+      return 0x8815
+    }
+    return format
+  },
+
+  getTextureType: function (gl, type) {
+    if (gl[this.versionProperty] !== 2) {
+      return type
+    }
+    if (type === HALF_FLOAT_OES) {
+      return gl.HALF_FLOAT
+    }
+    return type
+  }
+};
+
 var GL_SUBPIXEL_BITS = 0x0D50
 var GL_RED_BITS = 0x0D52
 var GL_GREEN_BITS = 0x0D53
@@ -1214,7 +1365,9 @@ var wrapLimits = function (gl, extensions) {
   if (readFloat) {
     var readFloatTexture = gl.createTexture()
     gl.bindTexture(GL_TEXTURE_2D, readFloatTexture)
-    gl.texImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 1, 1, 0, GL_RGBA, GL_FLOAT$1, null)
+
+    var internalFormat = gl2.getInternalFormat(gl, GL_RGBA, GL_FLOAT$1)
+    gl.texImage2D(GL_TEXTURE_2D, 0, internalFormat, 1, 1, 0, GL_RGBA, GL_FLOAT$1, null)
 
     var fbo = gl.createFramebuffer()
     gl.bindFramebuffer(GL_FRAMEBUFFER, fbo)
@@ -2212,90 +2365,6 @@ var isPow2$1 = function (v) {
   return !(v & (v - 1)) && (!!v)
 }
 
-var GL_DEPTH_COMPONENT$1 = 0x1902
-var GL_DEPTH_STENCIL$1 = 0x84F9
-var HALF_FLOAT_OES = 0x8D61
-
-var gl2Extensions = {
-  'WEBGL_depth_texture': {
-    'UNSIGNED_INT_24_8_WEBGL': 0x84FA
-  },
-  'OES_element_index_uint': {},
-  'OES_texture_float': {},
-  'OES_texture_float_linear': {},
-  'OES_texture_half_float': {
-    'HALF_FLOAT_OES': 0x8D61
-  },
-  'OES_texture_half_float_linear': {},
-  'OES_standard_derivatives': {},
-  'EXT_frag_depth': {},
-  'EXT_blend_minmax': {
-    'MIN_EXT': 0x8007,
-    'MAX_EXT': 0x8008
-  },
-  'EXT_shader_texture_lod': {}
-}
-
-var gl2 = {
-  gl2: function (gl, extensions) {
-    gl[this.versionProperty] = 2
-    for (var p in gl2Extensions) {
-      extensions[p.toLowerCase()] = gl2Extensions[p]
-    }
-
-    // mocks of draw buffers's functions
-    extensions['WEBGL_draw_buffers'] = {
-
-    }
-
-    // mocks of vao functions
-    extensions['OES_vertex_array_object'] = {
-
-    }
-  },
-
-  versionProperty: '___regl_gl_version___',
-
-  getInternalFormat: function (gl, format, type) {
-    if (gl[this.versionProperty] !== 2) {
-      return format
-    }
-    // webgl2
-    // reference:
-    // https://webgl2fundamentals.org/webgl/lessons/webgl-data-textures.html
-    if (format === GL_DEPTH_COMPONENT$1) {
-      // gl.DEPTH_COMPONENT24
-      return 0x81A6
-    } else if (format === GL_DEPTH_STENCIL$1) {
-      // gl.DEPTH24_STENCIL8
-      return 0x88F0
-    } else if (type === HALF_FLOAT_OES && format === gl.RGBA) {
-      // gl.RGBA16F
-      return 0x881A
-    } else if (type === HALF_FLOAT_OES && format === gl.RGB) {
-      // gl.RGB16F
-      return 0x881B
-    } else if (type === gl.FLOAT && format === gl.RGBA) {
-      // gl.RGBA32F
-      return 0x8814
-    } else if (type === gl.FLOAT && format === gl.RGB) {
-      // gl.RGB32F
-      return 0x8815
-    }
-    return format
-  },
-
-  getTextureType: function (gl, type) {
-    if (gl[this.versionProperty] !== 2) {
-      return type
-    }
-    if (type === HALF_FLOAT_OES) {
-      return gl.HALF_FLOAT
-    }
-    return type
-  }
-};
-
 var GL_COMPRESSED_TEXTURE_FORMATS = 0x86A3
 
 var GL_TEXTURE_2D$1 = 0x0DE1
@@ -2317,8 +2386,8 @@ var GL_UNSIGNED_SHORT_5_5_5_1$1 = 0x8034
 var GL_UNSIGNED_SHORT_5_6_5$1 = 0x8363
 var GL_UNSIGNED_INT_24_8_WEBGL$1 = 0x84FA
 
-var GL_DEPTH_COMPONENT = 0x1902
-var GL_DEPTH_STENCIL = 0x84F9
+var GL_DEPTH_COMPONENT$1 = 0x1902
+var GL_DEPTH_STENCIL$1 = 0x84F9
 
 var GL_SRGB_EXT = 0x8C40
 var GL_SRGB_ALPHA_EXT = 0x8C42
@@ -2397,8 +2466,8 @@ var CHANNELS_FORMAT = [
 var FORMAT_CHANNELS = {}
 FORMAT_CHANNELS[GL_LUMINANCE] =
 FORMAT_CHANNELS[GL_ALPHA] =
-FORMAT_CHANNELS[GL_DEPTH_COMPONENT] = 1
-FORMAT_CHANNELS[GL_DEPTH_STENCIL] =
+FORMAT_CHANNELS[GL_DEPTH_COMPONENT$1] = 1
+FORMAT_CHANNELS[GL_DEPTH_STENCIL$1] =
 FORMAT_CHANNELS[GL_LUMINANCE_ALPHA] = 2
 FORMAT_CHANNELS[GL_RGB] =
 FORMAT_CHANNELS[GL_SRGB_EXT] = 3
@@ -2439,7 +2508,7 @@ var FORMAT_SIZES_SPECIAL = []
 FORMAT_SIZES_SPECIAL[GL_RGBA4] = 2
 FORMAT_SIZES_SPECIAL[GL_RGB5_A1] = 2
 FORMAT_SIZES_SPECIAL[GL_RGB565] = 2
-FORMAT_SIZES_SPECIAL[GL_DEPTH_STENCIL] = 4
+FORMAT_SIZES_SPECIAL[GL_DEPTH_STENCIL$1] = 4
 
 FORMAT_SIZES_SPECIAL[GL_COMPRESSED_RGB_S3TC_DXT1_EXT] = 0.5
 FORMAT_SIZES_SPECIAL[GL_COMPRESSED_RGBA_S3TC_DXT1_EXT] = 0.5
@@ -2678,8 +2747,8 @@ function createTextureSet (
 
   if (extensions.webgl_depth_texture) {
     extend(textureFormats, {
-      'depth': GL_DEPTH_COMPONENT,
-      'depth stencil': GL_DEPTH_STENCIL
+      'depth': GL_DEPTH_COMPONENT$1,
+      'depth stencil': GL_DEPTH_STENCIL$1
     })
 
     extend(textureTypes, {
@@ -2774,8 +2843,8 @@ function createTextureSet (
         glenum === GL_ALPHA ||
         glenum === GL_LUMINANCE ||
         glenum === GL_LUMINANCE_ALPHA ||
-        glenum === GL_DEPTH_COMPONENT ||
-        glenum === GL_DEPTH_STENCIL ||
+        glenum === GL_DEPTH_COMPONENT$1 ||
+        glenum === GL_DEPTH_STENCIL$1 ||
         (extensions.ext_srgb &&
                 (glenum === GL_SRGB_EXT ||
                  glenum === GL_SRGB_ALPHA_EXT))) {
